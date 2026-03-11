eb745e1e057615f594ddb01a3d4108b941ecad35
A T <amythompson0325@gmail.com>
2026-03-10 21:42:25 -0400

    input logic

--- ChatApp.java
+++ ChatApp.java
@@ -6,6 +6,10 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import java.net.*;
+import java.io.*;
+import java.util.*;
 
 public class ChatApp extends Application {
     public void start(Stage primaryStage) {
@@ -17,11 +21,32 @@ public class ChatApp extends Application {
         TextField messageBox = new TextField();
         TextArea history = new TextArea();
         history.setEditable(false);
+        Button submit = new Button("Submit");
+        TextField ipField = new TextField();
+        TextField nameField = new TextField();
+        Label promptIP = new Label("IP");
+        Label promptName = new Label("Name");
         Button send = new Button("Send");
+        Label historyLabel = new Label("History");
 
-        grid.add(messageBox, 0, 0);
-        grid.add(send, 0, 1);
-        grid.add(history, 0, 3);
+
+        grid.add(promptIP, 0, 0);
+        grid.add(ipField, 0, 1);
+        grid.add(promptName, 0, 2);
+        grid.add(nameField, 0, 3);
+        grid.add(submit, 0, 4);
+        grid.add(messageBox, 0, 5);
+        grid.add(send, 0, 6);
+        grid.add(historyLabel, 0,  7);
+        grid.add(history, 0, 8);
+
+        submit.setOnAction(e -> {
+            String IP = ipField.getText();
+            String name = nameField.getText();
+            nameField.setEditable(false);
+            ipField.setEditable(false);
+        });
+
         send.setOnAction(e -> {
             String input = messageBox.getText();
             history.appendText(input + "\n");
@@ -31,6 +56,7 @@ public class ChatApp extends Application {
         primaryStage.setTitle("Chat App");
         primaryStage.setScene(scene);
         primaryStage.show();
+        primaryStage.setMaximized(true);
     }
 
     public static void main(String[] args) {
